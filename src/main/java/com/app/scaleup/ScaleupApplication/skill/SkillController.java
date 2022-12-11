afaf51128c7fa8260434941709a308a2a5cb33de
Dishant Gandhi <dishantg@amdocs.com>
2022-12-11 22:07:10 +0530

    Integrated ReactJs with implementation of /Skills Rest Fetch request

--- src/main/java/com/app/scaleup/ScaleupApplication/skill/SkillController.java
+++ src/main/java/com/app/scaleup/ScaleupApplication/skill/SkillController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin
 public class SkillController {
 
 	@Autowired
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 insert into Skill(id, name)
 values (2, 'C++');
+
+insert into Skill(id, name)
+values (1, 'JAVA');
